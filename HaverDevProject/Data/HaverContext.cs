4049ea7949d47259b847877778ed0dd65d3f55eb
Nigel Temple <you@example.com>
2024-01-21 14:21:05 -0500

    started adding models to context class

--- HaverDevProject/Data/HaverContext.cs
+++ HaverDevProject/Data/HaverContext.cs
@@ -1,4 +1,5 @@
 ﻿using HaverDevProject.Models;
+using HaverIndividualTest.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HaverDevProject.Data
@@ -9,5 +10,20 @@ namespace HaverDevProject.Data
         { 
         }
 
+        public DbSet<Defect> Defects { get; set; }
+        public DbSet<Drawing> Drawings { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Engineer> Engineers { get; set; }
+        public DbSet<EngineerDispositionType> EngineerDispositionTypes { get; set; }
+        public DbSet<FollowUpType> FollowUpTypes { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemDefect> ItemDefects { get; set; }
+        public DbSet<ItemDefectPhoto> ItemDefectPhotos { get; set; }
+        public DbSet<Ncr> Ncrs { get; set; }
+        public DbSet<OpDispositionType> OpDispositionTypes { get; set; }
+        public DbSet<ProcessApplicable> ProcessApplicables { get; set; }
+        public DbSet<ProductNumber> ProductNumbers { get; set; }
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+        // Add NewModels instead
     }
 }
