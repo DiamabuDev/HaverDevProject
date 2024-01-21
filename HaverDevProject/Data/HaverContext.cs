b2474fff8d6e96610dae5806d30a1eb1636a447e
Nigel Temple <you@example.com>
2024-01-21 13:17:24 -0500

    Created the classes necessary for seed data

---
+++ HaverDevProject/Data/HaverContext.cs
@@ -0,0 +1,13 @@
+﻿using HaverDevProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HaverDevProject.Data
+{
+    public class HaverContext : DbContext
+    {
+        public HaverContext(DbContextOptions<HaverContext> options) : base(options) 
+        { 
+        }
+
+    }
+}
---
+++ HaverDevProject/Data/HaverInitializer.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+
+namespace HaverDevProject.Data
+{
+    public static class HaverInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            HaverContext context = applicationBuilder.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<HaverContext>();
+
+            try
+            {
+                //context.Database.EnsureDeleted();
+                //context.Database.EnsureCreated();
+                //context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.GetBaseException().Message);
+            }
+        }
+    }
+}
