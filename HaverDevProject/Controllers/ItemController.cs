219fdbdfd8c9617776a229e2730588f05216f96d
jorge1180 <80490272+jorge1180@users.noreply.github.com>
2024-01-23 16:04:14 -0500

    Items Controller and Views created

---
+++ HaverDevProject/Controllers/ItemController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HaverDevProject.Data;
+using HaverDevProject.Models;
+
+namespace HaverDevProject.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly HaverNiagaraContext _context;
+
+        public ItemController(HaverNiagaraContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Item
+        public async Task<IActionResult> Index()
+        {
+            var haverNiagaraContext = _context.Items.Include(i => i.Supplier);
+            return View(await haverNiagaraContext.ToListAsync());
+        }
+
+        // GET: Item/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Supplier)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: Item/Create
+        public IActionResult Create()
+        {
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "SupplierCode");
+            return View();
+        }
+
+        // POST: Item/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ItemId,ItemNumber,ItemName,ItemDescription,SupplierId")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "SupplierCode", item.SupplierId);
+            return View(item);
+        }
+
+        // GET: Item/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "SupplierCode", item.SupplierId);
+            return View(item);
+        }
+
+        // POST: Item/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ItemId,ItemNumber,ItemName,ItemDescription,SupplierId")] Item item)
+        {
+            if (id != item.ItemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.ItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "SupplierCode", item.SupplierId);
+            return View(item);
+        }
+
+        // GET: Item/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Supplier)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Items == null)
+            {
+                return Problem("Entity set 'HaverNiagaraContext.Items'  is null.");
+            }
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+          return _context.Items.Any(e => e.ItemId == id);
+        }
+    }
+}
