ccdaebe62f687235ffb75827b366bdbfc6900646
DiamabuDev <diana.maldonado130@gmail.com>
2024-01-22 21:30:44 -0500

    created custom controllers and changed the connection string in haver Context and appsettings

--- HaverDevProject/Data/HaverNiagaraContext.cs
+++ HaverDevProject/Data/HaverNiagaraContext.cs
@@ -58,7 +58,7 @@ public partial class HaverNiagaraContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=HaverNiagara;Trusted_Connection=SSPI;encrypt=false;");
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HaverNiagara;Trusted_Connection=SSPI;encrypt=false;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
