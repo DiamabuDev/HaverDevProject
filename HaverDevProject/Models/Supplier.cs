3a996f20c52b79d0bab15493c5327b9b9d8f20d3
Victor Lopez <lopezchavezvictorh@gmail.com>
2024-01-25 10:59:46 -0500

    create supplier design update

--- HaverDevProject/Models/Supplier.cs
+++ HaverDevProject/Models/Supplier.cs
@@ -14,19 +14,24 @@ public partial class Supplier
     public int SupplierId { get; set; }
 
     [Required]
+    [Display(Name = "Code")]
     [Column("supplierCode")]
-    [StringLength(45)]
+    [StringLength(45, ErrorMessage = "Supplier Code cannot be more than 45 characters long.")]
     [Unicode(false)]
     public string SupplierCode { get; set; }
 
     [Required]
+    [Display(Name = "Name")]
     [Column("supplierName")]
-    [StringLength(45)]
+    [StringLength(45, ErrorMessage = "Supplier Name cannot be more than 45 characters long.")]
     [Unicode(false)]
     public string SupplierName { get; set; }
 
     [Column("supplierEmail")]
-    [StringLength(45)]
+    [Display(Name = "Email")]
+    [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Invalid email format.")]
+    [DataType(DataType.EmailAddress)]
+    [StringLength(45, ErrorMessage = "Supplier Name cannot be more than 45 characters long.")]
     [Unicode(false)]
     public string SupplierEmail { get; set; }
 
